33fc90438f043a8526c013df990def1f048047df
summer <3188891768@qq.com>
2024-04-10 17:54:56 +0800

    Fix bug in slowandfast.java

--- programmercarl/src/com/linklist/slowandfast.java
+++ programmercarl/src/com/linklist/slowandfast.java
@@ -3,24 +3,30 @@ public class slowandfast {
         
     }
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        //二者交叉走，先到的继续换个分叉走
-        //二者的分叉路一个多一个少，刚开始领先的，走完一圈来长的走，就把优势抹平了
-        //分叉部分记录为a 和 b [a + b = b + a]
         ListNode fast = headA;
-        ListNode slow = headB
+        ListNode slow = headB;
         
-        while(fast!=slow){
+        while(fast != slow){
+            //这里的判断条件是fast和slow都不为空，
+            //因为如果有一个为空，那么说明没有交点
+            //如果fast不为null，还没走到最后呢，那么继续走
+            //如果fast走到了最后，那么fast指向headB
+            //如果slow不为null，还没走到最后呢，那么继续走
+            //如果slow走到了最后，那么slow指向headA
 
-            if(fast.next==null){
-                fast.next = headB;
+            if(fast != null){
+                fast = fast.next;
+            } else {
+                fast = headB;
             }
-            if(slow.next = null){
-                slow.next = headA;
+            
+            if(slow != null){
+                slow = slow.next;
+            } else {
+                slow = headA;
             }
-            fast = fast.next;
-            slow = slow.next;
         }
+        //没交点，两个指针都指向null
         return fast;
     }
-    
 }
