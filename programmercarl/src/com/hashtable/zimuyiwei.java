6117bc284d678e69b99526accc73241e3a7521a4
summer <3188891768@qq.com>
2024-04-14 16:28:46 +0800

    字母异位三种尝试

--- programmercarl/src/com/hashtable/zimuyiwei.java
+++ programmercarl/src/com/hashtable/zimuyiwei.java
@@ -1,15 +1,26 @@
 package com.hashtable;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class zimuyiwei {
     public  static void  main(String[] args) {
-        int [] num1 ={1,2,3,4,5,6,7,8,9,10};
-        int [] num2 = {2,4,6,8,10,12,14,16,18,20};
+        int [] num1 ={1,2,2,3,4,5,6,7,8,9,10};
+        int [] num2 = {2,4,2,2,2,2,6,8,10,12,14,16,18,20};
         for(int num : intersection(num1,num2)){
             System.out.printf(num+" ");
         }
+        System.out.println();
+        for(int num : inter(num1,num2)){
+            System.out.printf(num+" ");
+        }
+        System.out.println();
+        for(int num : intersect(num1,num2)){
+            System.out.printf(num+" ");
+        }
+
 
     }
     public static  int [] intersection(int[] nums1, int[] nums2) {
@@ -39,6 +50,55 @@ public class zimuyiwei {
 
         
         return arr;
+    }
+    public static int [] inter(int [] num1, int [] num2){
+        int [] hash1 = new int [1002];
+        int [] hash2 = new int [1002];
+        for(int i : num1){
+            hash1[i]++;
+        }
+        for (int i : num2) {
+            hash2[i]++;
+        }
+        List<Integer> relist = new ArrayList<>();
+        for (int i = 0; i < 1002; i++) {
+            if(hash1[i] > 0 && hash2[i] > 0){
+                relist.add(i);
+            }
+
+        }
+        int index = 0;
+        int [] ans = new int[relist.size()];
+        for (int integer : relist) {
+            ans[index++] = integer;
+        }
+        return ans;
+
+    }
+    public static int [] intersect(int[] num1, int[] num2) {
+        int [] hash1 = new int [1002];
+        int [] hash2 = new int [1002];
+        for(int i : num1){
+            hash1[i]++;
+        }
+        for (int i : num2) {
+            hash2[i]++;
+        }
+        List<Integer> relist = new ArrayList<>();
+        for (int i = 0; i < 1002; i++) {
+            //两个中一个等于0时即终止判断,所以准入条件就是两个都不能为零
+            while(hash1[i] !=0 && hash2[i] !=0){
+                relist.add(i);
+                hash1[i]--;
+                hash2[i]--;
+            }   
+        }
+        int index = 0;
+        int [] ans = new int[relist.size()];
+        for (int integer : relist) {
+            ans[index++] = integer;
+        }
+        return ans;
     }
 
     
