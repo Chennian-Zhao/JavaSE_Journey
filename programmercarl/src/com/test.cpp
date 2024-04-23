dc10e6f1f3b8245a3b90d023a260e55a2699f700
summer <3188891768@qq.com>
2024-04-23 12:33:17 +0800

    快排模板

--- programmercarl/src/com/test.cpp
+++ programmercarl/src/com/test.cpp
@@ -1,33 +1,50 @@
 #include<iostream>
 #include<algorithm>
+#include<string>
 
 using namespace std;
 
 // 自定义比较函数
-bool compare(int a, int b) {
-    // 奇数在前，偶数在后
-    if (a % 2 != b % 2) {
-        return a % 2 > b % 2;
+bool compare(const string& s1, const string& s2) {
+    int count1 = 0, count2 = 0;
+
+    // 如果两个字符串的长度不同，那么短的字符串应该排在前面
+    if(s1.length() != s2.length())
+        return s1.length() < s2.length();
+    else {
+        // 如果两个字符串的长度相同，那么我们需要计算每个字符串中1的数量
+
+        // 计算s1中1的数量
+        for(int i = 0; i < s1.length(); i++)
+            if(s1[i] == '1')
+                count1++; 
+
+        // 计算s2中1的数量
+        for(int i = 0; i < s2.length(); i++)
+            if(s2[i] == '1')
+                count2++; 
+
+        // 如果1的数量不同，那么1的数量少的字符串应该排在前面
+        if(count1 != count2)
+            return count1 < count2;
+        else
+            // 如果1的数量也相同，那么我们按照字典序排序
+            return s1 < s2;
     }
-    // 奇数按从大到小排序
-    if (a % 2 == 1) {
-        return a > b;
-    }
-    // 偶数按从小到大排序
-    return a < b;
 }
 
 int main() {
-    int nums[10];
-
-    for (int i = 0; i < 10; ++i) {
-        cin >> nums[i];
-    }
-    // 使用自定义的比较函数对整数进行排序
-    sort(nums, nums + 10, compare);
-    // 输出排序后的整数
-    for (int i = 0; i < 10; ++i) {
-        cout << nums[i] << " ";
-    }
+    string inputStrings[100]; 
+    int n; 
+    // 首先输入一个整数n，表示字符串的个数
+    cin >> n;
+    // 根据n的值，读取n个字符串
+    for(int i = 0; i < n; i++)
+        cin >> inputStrings[i];
+    // 使用自定义的比较函数对字符串进行排序
+    sort(inputStrings, inputStrings + n, compare);
+    // 输出排序后的字符串
+    for(int i = 0; i < n; i++)
+        cout << inputStrings[i] << endl;
     return 0;
 }
